1aeb2a6b7d7beb64835566c77d31ce605ab89b0a
Mahendra Sri Sai Karthik Desaboyina <146872866+Mahendra-Karthik@users.noreply.github.com>
2024-10-19 23:12:24 +0530

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Config:
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///exam.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DEBUG = True
