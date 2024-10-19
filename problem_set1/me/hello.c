6163b41418b79084e003cb1ce4014d78f237b30e
Mahendra Sri Sai Karthik Desaboyina <146872866+Mahendra-Karthik@users.noreply.github.com>
2024-10-19 23:40:14 +0530

    Delete problem_set1/me directory

--- problem_set1/me/hello.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-
-int main() {
-    string s = get_string("What's your name? ");
-    printf("Hello, %s\n", s);
-    return 0;
-}
--- problem_set1/me/world/hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-#include<cs50.h>
-int main(){
-        printf("hello, world\n");
-        return 0;
-}
